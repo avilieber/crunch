6d0e1445d646bb2a3ba312bb19f6333757b1975f
Avi Lieberman <avilieberman@icloud.com>
2021-07-01 20:31:10 -0400

    try rename

--- setup.py
+++ setup.py
@@ -1,19 +1,8 @@
 from setuptools import setup
 
+NAME = "crunch"
+VERSION = "0.0.1"
+PACKAGES = ["crunch", "crunch.tests"]
+PACKAGE_DATA = {"crunch": ["tests/test_data/*", "tests/test_data/*/*"]}
 
-NAME="luc"
-VERSION="0.0.1"
-PACKAGES = ['luc', 'luc.tests']
-PACKAGE_DATA = {
-    'luc': [
-        'tests/test_data/*',
-        'tests/test_data/*/*'
-    ]
-}
-
-setup(
-    name=NAME,
-    version=VERSION,
-    packages=PACKAGES,
-    package_data=PACKAGE_DATA
-)
+setup(name=NAME, version=VERSION, packages=PACKAGES, package_data=PACKAGE_DATA)
