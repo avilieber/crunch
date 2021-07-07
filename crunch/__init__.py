e4599c744a33833609ac99583df46e48dfdbd3e0
Avi Lieberman <avilieberman@icloud.com>
2021-07-07 13:08:48 -0400

    version to delete

--- crunch/__init__.py
+++ crunch/__init__.py
@@ -1,3 +1,4 @@
 from .__main__ import AUDIT_NAME, OUTPUT_NAME, SONG_SHEET_NAME, main
+from ._version import __version__  # noqa: F401
 
 __all__ = ["SONG_SHEET_NAME", "OUTPUT_NAME", "AUDIT_NAME", "main"]
